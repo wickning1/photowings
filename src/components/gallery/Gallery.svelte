3f47e3f558a26f8a85cc1b7e460d16a5a3dc6573
Nick Wing <git@mythgaming.net>
2019-08-03 08:05:48 -0500

    Gallery: fix a bug with the layout calculation when reducing the number of columns

--- src/components/gallery/Gallery.svelte
+++ src/components/gallery/Gallery.svelte
@@ -19,17 +19,27 @@
   let savecolumns = 0
   let cardelements = {}
   let columnelements = []
+  let unsortedcolumn
   afterUpdate(async () => {
     await tick()
     if (savecolumns !== columns) { // only do work if number of columns has changed, afterUpdate triggers on resize
       const heights = Array.apply(null, Array(columns)).map(h => 0) // initializes heights to an array of zeroes
+      // elements in columns that have just been eliminated need to be placed back into the DOM
+      // in order to have a height - the easiest way to do that is to add them all back into unsorted
+      for (const card of Object.values(cardelements)) unsortedcolumn.append(card)
+      // collect all the card heights at this new column width
+      // we do this before we start the main sorting loop so that all our
+      // DOM reads are bundled up to avoid thrashing
       const cardheights = images.reduce((acc, curr) => { acc[curr.id] = cardelements[curr.id].clientHeight; return acc }, {})
+      // begin sorting images into columns
       for (const image of images) {
         // find the column with the smallest current height
         const colidx = heights.reduce((acc, curr, curridx) => curr < heights[acc] ? curridx : acc, 0)
         // move the current card to the chosen column
-        columnelements[colidx].appendChild(cardelements[image.id])
+        // console.log('moving', image.name, 'to column', colidx)
+        columnelements[colidx].append(cardelements[image.id])
         // record the height we just added to the chosen column
+        // do NOT read the height from the DOM, it would cause thrashing
         heights[colidx] += cardheights[image.id] + 10
       }
       savecolumns = columns
@@ -59,7 +69,7 @@
   {#each columnarray as col, idx}
     <div class="gallery-column" style="width: {100 / columns}%;" bind:this={columnelements[idx]}></div>
   {/each}
-  <div class="gallery-unsorted" style="width: {100 / columns}%;">
+  <div class="gallery-unsorted" style="width: {100 / columns}%;" bind:this={unsortedcolumn}>
     {#each images as image (image.id)}
       <CardComponent image={image} bind:topelement={cardelements[image.id]} />
     {/each}
