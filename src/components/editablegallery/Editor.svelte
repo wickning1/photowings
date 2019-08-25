40a794c2de52b4b1466154ae60a8f253a1b70023
Nick Wing <git@mythgaming.net>
2019-08-25 10:57:47 -0500

    style changes to the Editor and add a cancel button to dismiss

--- src/components/editablegallery/Editor.svelte
+++ src/components/editablegallery/Editor.svelte
@@ -3,6 +3,8 @@
   import MultiSelect from '../smartforms/inputs/public/MultiSelect'
   import SingleLine from '../smartforms/inputs/public/SingleLine'
   import Form from '../smartforms/core/Form'
+  import { createEventDispatcher } from 'svelte'
+  const dispatch = createEventDispatcher()
   async function validate (formdata) {
     console.log('validate', formdata)
     return {}
@@ -11,20 +13,29 @@
     console.log('submit')
     return {}
   }
+  function dismiss() {
+    dispatch('dismiss')
+  }
 </script>
 
 <style>
   :global(.gallery-editor) {
     background-color: white;
-    height: 90vh;
+    height: calc(100vh - 20px);
     width: 800px;
-    max-width: 90vw;
+    max-width: calc(100vw - 20px);
     padding: 20px;
     overflow-y: auto;
   }
+  .buttons {
+    display: flex;
+    flex-direction: row-reverse;
+    flex-wrap: wrap;
+    margin-top: 25px;
+  }
 </style>
 
-<Form preload={$editorpreload} submit={submit} validate={validate} className='gallery-editor'>
+<Form preload={$editorpreload} submit={submit} validate={validate} className='gallery-editor pure-form pure-form-stacked'>
   {#if $selectednumber === 1}
     <SingleLine name="title"/>
   {/if}
@@ -33,5 +44,8 @@
       <option value={tag.id}>{tag.name}</option>
     {/each}
   </MultiSelect>
-  <input type="submit" value="Update Images">
+  <div class="buttons">
+    <input type="submit" value="Update Images">
+    <input type="submit" on:click|preventDefault={dismiss} value="Cancel">
+  </div>
 </Form>
